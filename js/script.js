1f97f28ebb7332eab35e42fa7ef6753c5598727d
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-24 16:35:46 +0200

    Fix main-jumbotron, add basic animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+var phone = document.querySelector('.phone');
+var jumbotron = document.querySelector('.main-jumbotron');
+
+phone.addEventListener('click', function(event) {
+    jumbotron.classList.toggle('change-jumbotron');
+})

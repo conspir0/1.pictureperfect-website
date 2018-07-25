d6f454e4ab1db52cacc7c4470137df29028ffdfb
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-25 14:37:18 +0200

    Add animation to BOXES

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 'use strict';
 
+// Jumbotron
+
 var phone = document.querySelector('.phone');
 var jumbotron = document.querySelector('.main-jumbotron');
 
 phone.addEventListener('click', function(event) {
     jumbotron.classList.toggle('change-jumbotron');
 })
+
+// Boxes
+
+var link = document.querySelectorAll('.link');
+
+var linkHover = function(event) {
+    this.parentElement.classList.toggle('overlay');
+}
+
+for(var i = 0; i < link.length; i++) {
+    link[i].addEventListener('mouseenter', linkHover);
+    link[i].addEventListener('mouseleave', linkHover);
+};
